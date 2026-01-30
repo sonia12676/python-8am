ea3c90af568d236be6f1a308253746857fd9ec5e
sonia12676 <planthat763@gmail.com>
2026-01-30 16:37:08 +0545

    new changes

---
+++ controlstaement/nestedloop.py
@@ -0,0 +1,44 @@
+# for x in range(1,6):
+#     for a in range(x):
+#         print("*",end=" ")
+#     print()
+
+# for x in range(6,0,-1):
+#     for a in range(x):
+#        print("*",end=" ")
+#     print()
+    
+# for x in range(1,6):
+#     for a in range(x):
+#         print(x,end=" ")
+#     print()
+ 
+# for x in range(1,6):
+#     for a in range(1,x+1):
+#         print(a,end=" ")
+#     print()
+
+data=[
+    [12,34,56,78,97],
+    [23,45,67,89,90],
+    [34,56,78,90,12]
+]
+# sum_value
+# total_even_number   
+#total_odd_number  
+
+sum_value=0
+total_even_number=0
+total_odd_number=0
+
+for a in data:
+    for b in a:
+        sum_value+=b
+        if b%2==0:
+            total_even_number+=1
+        else:
+            total_odd_number+=1
+
+print(f"The total sum: {sum_value}")
+print(f"Total even numbers: {total_even_number} \t Total odd numbers: {total_odd_number}")
+ 
---
+++ controlstaement/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+for x in range(1,6):
+    for a in range(x):
+        print(x,end=" ")
+    print()
