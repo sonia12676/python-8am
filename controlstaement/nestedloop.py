941416bff34cc251677d789898ae5ab0b1230ef0
sonia12676 <planthat763@gmail.com>
2026-02-03 00:08:27 +0545

    functionday1

--- controlstaement/nestedloop.py
+++ controlstaement/nestedloop.py
@@ -27,18 +27,18 @@ data=[
 # total_even_number   
 #total_odd_number  
 
-sum_value=0
-total_even_number=0
-total_odd_number=0
+# sum_value=0
+# total_even_number=0
+# total_odd_number=0
 
-for a in data:
-    for b in a:
-        sum_value+=b
-        if b%2==0:
-            total_even_number+=1
-        else:
-            total_odd_number+=1
+# for a in data:
+#     for b in a:
+#         sum_value+=b
+#         if b%2==0:
+#             total_even_number+=1
+#         else:
+#             total_odd_number+=1
 
-print(f"The total sum: {sum_value}")
-print(f"Total even numbers: {total_even_number} \t Total odd numbers: {total_odd_number}")
+# print(f"The total sum: {sum_value}")
+# print(f"Total even numbers: {total_even_number} \t Total odd numbers: {total_odd_number}")
  
---
+++ function/main.py
@@ -0,0 +1,8 @@
+
+def total(numbers):
+    total_sum=0
+    for a in numbers:
+        total_sum+=a
+    print(total_sum)
+
+total([1,3,5,6])
