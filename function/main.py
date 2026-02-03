9d34f360cc9d05aaae6167d0542963adf0a7a62d
sonia12676 <planthat763@gmail.com>
2026-02-03 08:15:54 +0545

    function1_1

--- function/main.py
+++ function/main.py
@@ -1,8 +1,20 @@
 
-def total(numbers):
-    total_sum=0
-    for a in numbers:
-        total_sum+=a
-    print(total_sum)
+# def total(numbers):
+#     total_sum=0
+#     for a in numbers:
+#         total_sum+=a
+#     print(total_sum)
 
-total([1,3,5,6])
+# total([1,3,5,6])
+
+# *
+# def add(a,b):
+#     return a+b
+# def total(x,y,callback):
+#     print(callback(x,y))
+
+# total(6,7,add)
+
+# *
+# total=lambda x,y:x+y
+# print(total(8,8))
